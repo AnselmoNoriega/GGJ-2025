833de39a0b27fb613a024fcd0593323c887e6592
AnselmoNoriega <anselmonoriega3@gmail.com>
2025-01-22 13:52:54 -0800

    new project started

--- GGJ_2025/Source/GGJ_2025Editor.Target.cs
+++ GGJ_2025/Source/GGJ_2025Editor.Target.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class GGJ_2025EditorTarget : TargetRules
 {
-	public GGJ_2025EditorTarget( TargetInfo Target) : base(Target)
+	public GGJ_2025EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
